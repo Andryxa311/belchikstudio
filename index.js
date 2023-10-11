a59ce7de0d533e8730e876f7b70eee8ec572e988
Andryxa311 <andrey00manager@gmail.com>
2023-10-11 23:08:01 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.js
    	modified:   package.json

--- index.js
+++ index.js
@@ -7,44 +7,57 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '6681738989:AAFNhCWDYoVwIelUFUD1zR6GhQb-olVPp_M';
 const bot = new TelegramBot(token, { polling: true });
 
+let zayavki = 0
+let metod
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.post('/api', (request, response) => {
+    zayavki++
+    console.log(request.body['com_metod']);
+
+    if (request.body['com_metod'] == 'tg') {
+        metod = "Телеграмм"
+    } else if (request.body['com_metod'] == 'wts') {
+        metod = "Вотсапп"
+    } else if (request.body['com_metod'] == 'viber') {
+        metod = "Вибер"
+    } else if (request.body['com_metod'] == 'mail') {
+        metod = "Почта"
+    }
+    bot.sendMessage(530751804,`Заявка: ${zayavki}\n\nИмя клиента: ${request.body['name']}\nТелефон клиента: ${request.body['phone']}\n  Способ связи: ${metod}`)
+    .catch(err => {
+        console.log(err.code);
+        console.log(err.response.body);
+    })
+    response.status(201)
+    response.end()
+})
+app.listen(3000)
+
+
+bot.sendMessage(530751804, 'Сервер перезагрузился, проверь на всякий случай.\n\n У тебя большой член!')
+.catch(err => {
+    console.log(err.code);
+    console.log(err.response.body);
+})
+
+
+
+
+
+
+
+
+
 
 // app.get('/api', (req,res) => {
 //     res.status(201).json(jsson)
 // })
-
-app.use(cors());
 // app.get('*', (request, response) => {
 //     response.status(200)
 //     bot.sendMessage(530751804,"Сообщение дурака").catch(err => {
-//         console.log(err.code);
-//         console.log(err.response.body);
-//     })
-// })
-app.post('*', (request, response) => {
-    response.status(200)
-    bot.sendMessage(530751804,"Сообщение дурака").catch(err => {
-        console.log(err.code);
-        console.log(err.response.body);
-    })
-})
-app.listen(3000)
-
-// bot.on('message', async massage => {
-//     const text = massage.text;
-//     const chatID = massage.from.id;
-//     const is_bot = massage.from.is_bot;
-//     const user_name = massage.from.username;
-//     const date = massage.date;
-//     const massageID = massage.message_id;
-    
-//     console.log(text);
-//     console.log(chatID);
-//     console.log(is_bot);
-//     console.log(user_name);
-//     console.log(date);
-//     console.log(massageID);
-
-//     bot.sendMessage(chatID,"Ну че здарова ебать").catch(err => {
 //         console.log(err.code);
 //         console.log(err.response.body);
 //     })
